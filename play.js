85f69f09be32ff57d5b971b970e49e1ded546278
Bram van der Veen <96aa48@gmail.com>
2016-06-19 22:23:15 +0200

    Fixed issues with mplayer intergration

--- play.js
+++ play.js
@@ -1,6 +1,7 @@
 const readline = require('readline-sync');
-const playmusic = new (require('playmusic'))();
+const mplayer = require('child_process').spawn;
 const cli = require('cli');
+const os = require('os');
 
 function mplayerArgs (filename, isPlaylist) {
   var audioEngines = {
@@ -21,23 +22,13 @@ function album(playlistFile) {
   play(playlistFile, true);
 }
 
+//FIXME: For some reason mplayer quits after a few seconds of playing a song.
 function track(file, playlist) {
   playlist = !!playlist; // default to false
 
-  var player = mplayer('mplayer', mplayerArgs(file, playlist));
+  var player = mplayer('mplayer', mplayerArgs(file, playlist), {stdio: 'inherit'});
   var isfiltered = false;
   console.log('Playing ' + path.basename(file) + '\n');
-
-  player.stdout.on('data', (data) => {
-    if (data.toString().substr(0,2) == 'A:' && !isfiltered) {
-      player.stdout.pipe(process.stdout);
-      isfiltered = true;
-    }
-  });
-
-  // FIXME: In order for the input piping to mplayer to work I need to require this.
-  require('readline').createInterface({input : process.stdin, output : process.stdout});
-  process.stdin.pipe(player.stdin);
 
   player.on('error', (data) => {
     cli.fatal('There was an error playing your song, maybe you need to install mplayer?');
